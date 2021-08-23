f787628fb74ad35e9b86eb987c12154bc50c9b37
Michiel Gerritsen <michiel@controlaltdelete.nl>
2021-08-23 14:54:58 +0200

    Fixed 2.4.3, added example test

---
+++ Test/ExampleTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *    ______            __             __
+ *   / ____/___  ____  / /__________  / /
+ *  / /   / __ \/ __ \/ __/ ___/ __ \/ /
+ * / /___/ /_/ / / / / /_/ /  / /_/ / /
+ * \______________/_/\__/_/   \____/_/
+ *    /   |  / / /_
+ *   / /| | / / __/
+ *  / ___ |/ / /_
+ * /_/ _|||_/\__/ __     __
+ *    / __ \___  / /__  / /____
+ *   / / / / _ \/ / _ \/ __/ _ \
+ *  / /_/ /  __/ /  __/ /_/  __/
+ * /_____/\___/_/\___/\__/\___/
+ *
+ */
+
+namespace MichielGerritsen\Magento2ExtensionIntegrationTest\Test\Integration;
+
+class ExampleTest extends CustomTestCase
+{
+    /**
+     * The only goal of this test is to check if test can be run successful.
+     */
+    public function testSuccess()
+    {
+        $this->assertTrue(true);
+    }
+}
